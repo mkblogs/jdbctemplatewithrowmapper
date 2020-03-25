399b26fdc6f9e65ff634dc99cc13ddcf908008a4
Maria Kumar <tech.mkblog@gmail.com>
2020-03-25 17:41:47 +0530

    Initial commit

---
+++ src/main/java/com/tech/mkblogs/JdbctemplatewithrowmapperApplication.java
@@ -0,0 +1,13 @@
+package com.tech.mkblogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbctemplatewithrowmapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbctemplatewithrowmapperApplication.class, args);
+	}
+
+}
